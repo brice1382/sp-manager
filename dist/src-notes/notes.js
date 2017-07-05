7a0ab13d3b13e2225834fafc05f0c780f6945580
brice1382 <brice721@hotmail.com>
2017-07-05 09:12:49 -0400

    Delete notes.js

--- dist/src-notes/notes.js
+++
@@ -1,4 +0,0 @@
-/** I left off starting to add my javascript to the src dir.
-The majority of what I have has already been added.
-Now its time to start writing more for it.
-Note Created On: Tuesday, July 4, 2017, 6:04:38 AM EDT */
