d578fd8c49286d6c4416b6d6e9c55233800172ca
brice1382 <brice721@hotmail.com>
2017-07-05 09:15:29 -0400

    Add dist Folder
    dist Folder Initial Commit

---
+++ dist/sp-manager.js
@@ -0,0 +1,50 @@
+(function() {
+	'use strict';
+
+
+/**
+ * Provides a means of attaching animations only if user 'Is Flashy'
+ * Meaning only if they navigate to a certain page by way of a specific button
+ */
+
+angular
+    .module('sp-manager', [
+        'sp-manager.roots'
+    ]);
+
+angular
+    .module('sp-manager.roots', [])
+    .provider('FlashManager', function () {
+
+        this.$get = ['$rootScope', function ($rootScope) {
+
+
+            $rootScope.isFlashy = false;
+
+            function flashify() {
+                $rootScope.isFlashy = true;
+                var store = 'true';
+                localStorage.setItem('flash', store);
+            }
+
+            function unFlashify() {
+                $rootScope.isFlashy = false;
+                localStorage.removeItem('flash');
+            }
+
+            function checkOnRefresh() {
+                $rootScope.$on('$viewContentLoaded', function() {
+                    if (localStorage.getItem('flash') === 'true') {
+                        flashify();
+                    }
+                })
+            }
+
+            return {
+                flashify: flashify,
+                unFlashify: unFlashify,
+                checkOnRefresh: checkOnRefresh
+            }
+        }];
+    });
+}());
---
+++ dist/src-notes/sp-manager.js
@@ -0,0 +1,54 @@
+/** I left off starting to add my javascript to the src dir.
+The majority of what I have has already been added.
+Now its time to start writing more for it.
+Note Created On: Tuesday, July 4, 2017, 6:04:36 AM EDT */
+(function() {
+	'use strict';
+
+
+/**
+ * Provides a means of attaching animations only if user 'Is Flashy'
+ * Meaning only if they navigate to a certain page by way of a specific button
+ */
+
+angular
+    .module('sp-manager', [
+        'sp-manager.roots'
+    ]);
+
+angular
+    .module('sp-manager.roots', [])
+    .provider('FlashManager', function () {
+
+        this.$get = ['$rootScope', function ($rootScope) {
+
+
+            $rootScope.isFlashy = false;
+
+            function flashify() {
+                $rootScope.isFlashy = true;
+                var store = 'true';
+                localStorage.setItem('flash', store);
+            }
+
+            function unFlashify() {
+                $rootScope.isFlashy = false;
+                localStorage.removeItem('flash');
+            }
+
+            function checkOnRefresh() {
+                $rootScope.$on('$viewContentLoaded', function() {
+                    if (localStorage.getItem('flash') === 'true') {
+                        flashify();
+                    }
+                })
+            }
+
+            return {
+                flashify: flashify,
+                unFlashify: unFlashify,
+                checkOnRefresh: checkOnRefresh
+            }
+        }];
+    });
+}());
