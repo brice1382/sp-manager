1a0cdc3f036c260349e18eaea76b9dbfaf90884b
brice1382 <brice721@gmail.com>
2017-07-06 08:09:07 -0400

    Remove Private for Bower Register
    Removed label private to be able to register with bower.

--- dist/src-notes/notes.js
+++ dist/src-notes/notes.js
@@ -1,4 +1,4 @@
-/** I left off starting to add my javascript to the src dir.
-The majority of what I have has already been added.
-Now its time to start writing more for it.
-Note Created On: Thursday, July 6, 2017, 3:58:01 AM EDT */
+/** 
+
+
+Note Created On: Thursday, July 6, 2017, 8:06:28 AM EDT */
